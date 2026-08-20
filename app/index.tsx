29f07287bcfd30a379e986c7b328d5f981fd96ee
PolyanaValtrich <polyana.valtrich@aluno.senai.br>
2026-08-20 11:32:47 -0300

    Login em andamento

--- app/index.tsx
+++ app/index.tsx
@@ -4,9 +4,7 @@ import "../global.css";
 import Login from "@/pages/Login/Login";
 const App = () => {
   return (
-    <View className=" flex-1 bg-[#0007a0]">
       <Login />
-    </View>
   );
 };
 
